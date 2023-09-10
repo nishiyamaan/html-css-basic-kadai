1676859d14e73bee1604d884e14b8ba0dbbf2c09
Nishiyama_vscode <jutay134@gmail.com>
2023-09-10 09:58:08 +0900

    i

--- django/myproject/crud/views.py
+++ django/myproject/crud/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Product
+from django.urls import reverse_lazy
+
 # Create your views here.
 
+
 class TopView(TemplateView):
-    template_name = "top.html"
+    template_name = "crud/top.html"
+
+
+class ProductListView(ListView):
+    template_name = "crud/product_list.html"
+    model = Product
+    paginate_by = 3
+
+
+class ProductCreateView(CreateView):
+    template_name = "crud/product_form.html"
+    model = Product
+    fields = "__all__"
+
+
+class ProductUpdateView(UpdateView):
+    template_name = "crud/product_update_form.html"
+    model = Product
+    fields = "__all__"
+
+
+class ProductDeleteView(DeleteView):
+    template_name = "crud/confirm_delete.html"
+    model = Product
+    success_url = reverse_lazy("list")
+
+
+class ProductDitailView(DetailView):
+    template_name = "crud/product_detail.html"
+    model = Product
--- django/myproject/myproject/urls.py
+++ django/myproject/myproject/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crud import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.TopView.as_view(), name="top"),
+    path("admin/", admin.site.urls),
+    path("", views.TopView.as_view(), name="top"),
+    path("crud/", views.ProductListView.as_view(), name="list"),
+    path("crud/new/", views.ProductCreateView.as_view(), name="new"),
+    path("crud/edit/<int:pk>", views.ProductUpdateView.as_view(), name="edit"),
+    path("crud/delete/<int:pk>", views.ProductDeleteView.as_view(), name="delete"),
+    path("crud/detail/<int:pk>", views.ProductDitailView.as_view(), name="detail"),
+    # path('', views.ProductListView.as_view(), name='list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
